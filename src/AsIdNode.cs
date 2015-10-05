bc2ca07994ff4a74a6432ee4b2c911fef4e8a23a
smakhtin <algariol@gmail.com>
2015-10-05 16:33:48 +0300

    Added AsIdNode.cs

---
+++ src/AsIdNode.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using VVVV.PluginInterfaces.V2;
+
+namespace VVVV.Nodes.V
+{
+	[PluginInfo(Name = "AsId", Category = "String", Author = "alg", Tags = "data visualization", Help = "Converts string to lower case letters and digits only, so it can be used as ID")]
+	public class AsIdNode : IPluginEvaluate
+	{
+		[Input("Input", DefaultString = "text")] 
+		public IDiffSpread<string> FInput;
+
+		[Output("Output")]
+		public ISpread<string> FOutput; 
+		
+		public void Evaluate(int spreadMax)
+		{
+			FOutput.SliceCount = spreadMax;
+
+			if(!FInput.IsChanged) return;
+
+			for (var i = 0; i < spreadMax; i++)
+			{
+				FOutput[i] = new String(FInput[i].Where(Char.IsLetterOrDigit).ToArray()).ToLower();
+			}
+		}
+	}
+}
