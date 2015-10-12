5f2398ef0fdb3310683db83adcad4dee0757aa51
Vadim Smakhtin <algariol@gmail.com>
2015-10-12 14:20:24 +0300

    Moved JsonPathNode to Json.net Path API

--- src/V/Json/JsonPathNode.cs
+++ src/V/Json/JsonPathNode.cs
@@ -27,7 +27,6 @@ namespace VVVV.Nodes.V.Json
 		[Import]
 		protected ILogger FLogger;
 
-		private readonly List<List<string>> FData = new List<List<string>>(1);
 		private readonly JsonPathContext FParser = new JsonPathContext { ValueSystem = new JsonNetValueSystem() };
 
 		public void Evaluate(int spreadMax)
@@ -36,20 +35,22 @@ namespace VVVV.Nodes.V.Json
 
 			if(FParseIn[0] || FQueryIn.IsChanged)
 			{
-				FData.Clear();
 				for (var i = 0; i < spreadMax; i++)
 				{
 					try
 					{
-						var values = FParser.SelectNodes(FJObjectIn[i], FQueryIn[i]).Select(node => node.Value);
-						var list = values.ToList();
+					    var values = FJObjectIn[i].SelectTokens(FQueryIn[i]).Select(token => token.Value<string>()).ToList();
 
-						FData.Add(new List<string>());
-
-						for (var j = 0; j < values.Count(); j++)
-						{
-							FData[i].Add(list[j].ToString());
-						}
+                        if(values.Any())
+					    {
+                            FDataOut[i].SliceCount = values.Count();
+                            FDataOut[i].AssignFrom(values);
+					    }
+                        else
+                        {
+                            FDataOut[i].SliceCount = 0;
+                        }
+					    
 					}
 					catch (Exception ex)
 					{
@@ -58,13 +59,6 @@ namespace VVVV.Nodes.V.Json
 						FLogger.Log(LogType.Error, ex.ToString());
 					}
 				}
-			}
-
-			if(FData.Count == 0) return;
-
-			for (var i = 0; i < spreadMax; i++)
-			{
-				FDataOut[i].AssignFrom(FData[i]);
 			}
 		}
 	}
