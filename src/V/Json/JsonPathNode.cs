f0ffe6ff59f7792e20cdea929f156a59e0d3f185
Vadim Smakhtin <algariol@gmail.com>
2015-10-13 20:33:30 +0300

    Fixed JsonPath

--- src/V/Json/JsonPathNode.cs
+++ src/V/Json/JsonPathNode.cs
@@ -27,6 +27,7 @@ namespace VVVV.Nodes.V.Json
 		[Import]
 		protected ILogger FLogger;
 
+		private readonly List<List<string>> FData = new List<List<string>>(1);
 		private readonly JsonPathContext FParser = new JsonPathContext { ValueSystem = new JsonNetValueSystem() };
 
 		public void Evaluate(int spreadMax)
@@ -35,27 +36,35 @@ namespace VVVV.Nodes.V.Json
 
 			if(FParseIn[0] || FQueryIn.IsChanged)
 			{
+				FData.Clear();
 				for (var i = 0; i < spreadMax; i++)
 				{
 					try
 					{
-					    var values = FParser.SelectNodes(FJObjectIn[i], FQueryIn[i]).Select(node => (string) node.Value).ToList();
+						var values = FParser.SelectNodes(FJObjectIn[i], FQueryIn[i]).Select(node => node.Value);
+						var list = values.ToList();
 
-					    if (values.Any())
-					    {
-					        FDataOut[i].SliceCount = values.Count();
-                            FDataOut[i].AssignFrom(values);
-					    }
-					    else
-					    {
-					        FDataOut[i].SliceCount = 0;
-					    }
+						FData.Add(new List<string>());
+
+						for (var j = 0; j < values.Count(); j++)
+						{
+							FData[i].Add(list[j].ToString());
+						}
 					}
 					catch (Exception ex)
 					{
+						//FLogger.Log(LogType.Error, "Query is wrong at slice " + i);
+						
 						FLogger.Log(LogType.Error, ex.ToString());
 					}
 				}
+			}
+
+			if(FData.Count == 0) return;
+
+			for (var i = 0; i < spreadMax; i++)
+			{
+				FDataOut[i].AssignFrom(FData[i]);
 			}
 		}
 	}
